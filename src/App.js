7d0a45e00fec33720b9df6340861a7872c2cd25c
asouthern729 <andrew.southern@franklintn.gov>
2022-01-09 11:10:15 -0600

    Creating routes using react-router-dom version 6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/FeedbackForm'
+import AboutPage from './pages/AboutPage'
+
 import FeedbackData from './data/FeedbackData'
 
 
@@ -23,17 +26,28 @@ function App() {
     }
   }
 
+  /* react-router-dom version 6 requires <Route> be wrapped in <Routes> parent
+  component prop replaced with element */
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList 
-        feedback={feedback}
-        handleDelete={deleteFeedback} />
+        <Routes>
+          <Route exact path="/" element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList 
+              feedback={feedback}
+              handleDelete={deleteFeedback} />
+            </>
+          }>
+          </Route>
+
+          <Route exact path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
