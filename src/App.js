a3ebb680384a2adc34860f5591f1337d8d99aac3
asouthern729 <andrew.southern@franklintn.gov>
2022-01-10 13:02:46 -0600

    Moving function to Context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import { v4 as uuidv4 } from 'uuid'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
@@ -9,25 +7,7 @@ import AboutIconLink from './components/AboutIconLink'
 import AboutPage from './pages/AboutPage'
 import { FeedbackProvider } from './context/FeedbackContext'
 
-import FeedbackData from './data/FeedbackData'
-
-
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
-
-  const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    /* Spread operator adds existing feedback to global feedback state,
-    newFeedback object is appended to existing feedback */
-    setFeedback([newFeedback, ...feedback])
-  }
-
-  const deleteFeedback = (id) => {
-    if(window.confirm('Are you sure you want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
-  }
-
   /* react-router-dom version 6 requires <Route> be wrapped in <Routes> parent
   component prop replaced with element */
   return (
@@ -38,10 +18,9 @@ function App() {
           <Routes>
             <Route exact path="/" element={
               <>
-                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackForm />
                 <FeedbackStats />
-                <FeedbackList 
-                handleDelete={deleteFeedback} />
+                <FeedbackList />
               </>
             }>
             </Route>
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,18 +1,48 @@
 import { createContext, useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
+
+  /* State */
   const [feedback, setFeedback] = useState([
     {
       id: 1,
-      text: 'This item is from context',
+      text: 'This is feedback item 1',
       rating: 10
+    },
+    {
+      id: 2,
+      text: 'This is feedback item 2',
+      rating: 9
+    },
+    {
+      id: 3,
+      text: 'This is feedback item 3',
+      rating: 7
     }
   ])
 
+  /* Delete feedback function */
+  const deleteFeedback = (id) => {
+    if(window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id))
+    }
+  }
+
+  /* Add feedback function */
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+    /* Spread operator adds existing feedback to global feedback state,
+    newFeedback object is appended to existing feedback */
+    setFeedback([newFeedback, ...feedback])
+  }
+
   return <FeedbackContext.Provider value={{
-    feedback
+    feedback,
+    deleteFeedback,
+    addFeedback
   }}>
     {children}
   </FeedbackContext.Provider>
